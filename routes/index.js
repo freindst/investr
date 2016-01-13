4bcc2171bff129a18b897b4bd16ffd89192e5a94
freindst <yiming.liao@cuw.edu>
2016-01-13 10:55:23 -0600

    add historical data

--- routes/index.js
+++ routes/index.js
@@ -78,8 +78,20 @@ router.get('/log', function(req, res) {
 })
 
 router.get('/test/', function(req, res) {
-	console.log(schedule_list);
-	res.send("done");
+	var query = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22YHOO%22%20and%20startDate%20%3D%20%222009-09-11%22%20and%20endDate%20%3D%20%222010-03-10%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+	var json_obj = JSON.parse(Get(query));
+	var stock = json_obj.query.results.quote;
+	res.send(stock);
+});
+
+router.post('/historicaldata/', function(req,res) {
+	var symbol = req.body.symbol;
+	var startdate = req.body.startdate;
+	var enddate = req.body.enddate;
+	var query = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22' + symbol + '%22%20and%20startDate%20%3D%20%22' + startdate + '%22%20and%20endDate%20%3D%20%22' + enddate + '%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+	var json_obj = JSON.parse(Get(query));
+	var stock = json_obj.query.results.quote;
+	res.send(stock);
 })
 
 function portfolio(transaction)
