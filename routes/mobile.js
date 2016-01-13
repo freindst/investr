0902109a4fd84d496491a4954a9aa1ccd06834d5
freindst <yiming.liao@cuw.edu>
2016-01-13 10:56:48 -0600

    update historical date to mobile router

--- routes/mobile.js
+++ routes/mobile.js
@@ -35,6 +35,17 @@ router.get('/news/:stock_symbol', function(req, res){
 		});
 })
 
+//yahoo finance historical data
+router.post('/historicaldata/', function(req,res) {
+	var symbol = req.body.symbol;
+	var startdate = req.body.startdate;
+	var enddate = req.body.enddate;
+	var query = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22' + symbol + '%22%20and%20startDate%20%3D%20%22' + startdate + '%22%20and%20endDate%20%3D%20%22' + enddate + '%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+	var json_obj = JSON.parse(Get(query));
+	var stock = json_obj.query.results.quote;
+	res.send(stock);
+})
+
 //HTTP Request POST join a new game. Parameter: user_id, game_id
 router.post('/joinGame', function(req, res) {
 	var user_id = req.body.user_id;
